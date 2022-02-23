65c6cc0977b2f5c692fe2f8ee5d969e36840bfd4
Moshe <moisherosenzweig@gmail.com>
2022-02-23 10:20:20 -0500

    About__Me__Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "salmon" }}>
+        {name} is a Software engineer and <br></br>Blockchain Enthusiast <br></br> <br></br>from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
